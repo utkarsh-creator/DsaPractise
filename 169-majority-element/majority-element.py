8f7a50532d7f0f88330257e0070b87626bc39ce2
Utkarsh Tyagi <65786764+utkarsh-creator@users.noreply.github.com>
2024-07-27 02:10:49 +0530

    Time: 160 ms (93.03%) | Memory: 18.1 MB (14.98%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,27 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        unmap={}
-        majcount=len(nums)//2
+        candidate = None
+        count = 0
         for num in nums:
-            if num in unmap:
-                unmap[num]+=1
+            if count == 0:
+                candidate = num
+                count = 1
+            elif num == candidate:
+                count += 1
             else:
-                unmap[num]=1
-            if unmap[num]>majcount:
-                return num
+                count -= 1
+        return candidate
+
+            
+
+
+
+        # unmap={}
+        # majcount=len(nums)//2
+        # for num in nums:
+        #     if num in unmap:
+        #         unmap[num]+=1
+        #     else:
+        #         unmap[num]=1
+        #     if unmap[num]>majcount:
+        #         return num
