3d9bf8b977139c590f36a2f583509e1848da0181
Utkarsh Tyagi <65786764+utkarsh-creator@users.noreply.github.com>
2024-08-04 15:56:39 +0530

    Time: 31 ms (82.47%) | Memory: 16.7 MB (11.24%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        stack = []
+
+        for char in s:
+            if char in bracket_map:
+                if stack:
+                    top_element = stack.pop()
+                else:
+                    top_element = '#'
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
