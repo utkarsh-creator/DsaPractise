45048924d9243856a4b865d0a0c45a798251bbec
Utkarsh Tyagi <65786764+utkarsh-creator@users.noreply.github.com>
2024-10-23 16:37:24 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (93.63%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -7,22 +7,14 @@ class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
-
-        # Initialize pointers for odd and even nodes
         odd = head
         even = head.next
-        even_head = even  # To reconnect at the end
+        even_head = even 
 
         while even and even.next:
-            # Connect odd nodes together
             odd.next = even.next
             odd = odd.next
-
-            # Connect even nodes together
             even.next = odd.next
             even = even.next
-
-        # Link the end of odd nodes to the head of even nodes
         odd.next = even_head
-
         return head
